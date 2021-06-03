76063e8fce1420017de5ec52bb2af3318407e22c
Dmytro Aponasenko <33687161+dmytroap@users.noreply.github.com>
2021-06-03 14:48:20 +0300

    Fix type method (#8)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const ts_preset = require('ts-jest/jest-preset');
 const puppeteer_preset = require('jest-puppeteer/jest-preset');
 
--- src/element/actions/clearInput.ts
+++ src/element/actions/clearInput.ts
@@ -7,6 +7,7 @@ import { click } from './click';
  * Before clearing the value method performs click on the element.
  * After clicking on the input it clears the data inside by selecting all text and deleting it.
  * If a selector was passed then the method tries to find the element and only then clears the value.
+ * In order to clear the input a page object is needed, so if the context is `Frame` then an additional `page` argument should be passed.
  *
  * @category Element Actions
  */
@@ -14,7 +15,10 @@ export async function clearInput(
   context: Page | Frame,
   selectorOrElement: string | ElementHandle,
   searchElementOptions?: SearchElementOptions,
+  page?: Page,
 ): Promise<void> {
+  const pageContext = getPageContext(context, page);
+
   const element = await getElement(
     context,
     selectorOrElement,
@@ -23,9 +27,27 @@ export async function clearInput(
 
   await click(context, element);
 
-  await page.keyboard.press('Home');
-  await page.keyboard.down('Shift');
-  await page.keyboard.press('End');
-  await page.keyboard.up('Shift');
-  await page.keyboard.press('Backspace');
+  await pageContext.keyboard.press('Home');
+  await pageContext.keyboard.down('Shift');
+  await pageContext.keyboard.press('End');
+  await pageContext.keyboard.up('Shift');
+  await pageContext.keyboard.press('Backspace');
+}
+
+function getPageContext(context: Page | Frame, page: Page | undefined): Page {
+  if (!isFrame(context)) {
+    return context as Page;
+  }
+
+  if (!page) {
+    throw new Error(
+      `The page argument should be passed when the Frame is a context`,
+    );
+  }
+
+  return page;
+}
+
+function isFrame(context: Page | Frame): boolean {
+  return 'name' in context ? true : false;
 }
