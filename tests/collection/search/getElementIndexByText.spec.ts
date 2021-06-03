a920d2cf96af74914690db2cde4086a236fd1762
Dmytro Aponasenko <dmytroa@wix.com>
2021-06-03 10:05:07 +0300

    Add test for already found elements collection

--- tests/collection/search/getElementIndexByText.spec.ts
+++ tests/collection/search/getElementIndexByText.spec.ts
@@ -1,27 +1,49 @@
 import { getElementIndexByText } from '../../../src/collection/search/getElementIndexByText';
 
 describe('Collection Search: getElementIndexByText()', () => {
+  const headerSelector = 'tr th';
+
   beforeAll(async () => {
     await page.goto('http://the-internet.herokuapp.com/challenging_dom');
   });
 
   it('should find the index of the table header by text', async () => {
     const expectedIndex = 4;
+    const searchText = 'Amet';
 
-    const searchedIndex = await getElementIndexByText('Amet', page, 'tr th');
+    const searchedIndex = await getElementIndexByText(
+      searchText,
+      page,
+      headerSelector,
+    );
 
     expect(searchedIndex).toBe(expectedIndex);
   });
 
   it('should find the index of the table header by text ignoring a case', async () => {
     const expectedIndex = 5;
+    const searchText = 'diceret';
 
     const searchedIndex = await getElementIndexByText(
-      'diceret',
+      searchText,
       page,
-      'tr th',
+      headerSelector,
       {},
       true,
+    );
+
+    expect(searchedIndex).toBe(expectedIndex);
+  });
+
+  it('should find the index of the table header by text using the existing elements collection', async () => {
+    const expectedIndex = 1;
+    const searchText = 'Ipsum';
+    const elements = await page.$$(headerSelector);
+
+    const searchedIndex = await getElementIndexByText(
+      searchText,
+      page,
+      elements,
     );
 
     expect(searchedIndex).toBe(expectedIndex);
