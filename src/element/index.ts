7e04256bbc79e5cddbb6773feccd824c9e8c76b4
yaroslavm <yaroslavm@wix.com>
2023-02-15 18:58:48 +0200

    add internal element base driver #pr

--- src/element/index.ts
+++ src/element/index.ts
@@ -1,4 +1,4 @@
-export * from './elementBaseDriver';
+export * from './baseDrivers/elementBaseDriver';
 export * from './evaluateOnSelectorOrElement';
 export * from './getAttribute';
 export * from './getComputedStyle';
@@ -63,3 +63,4 @@ export * from './waits/waitForElementToBeUnChecked';
 export * from './waits/waitForElementToBeChecked';
 export * from './waits/waitForElementToHaveAttribute';
 export * from './waits/waitForScopedSelector';
+export * from './baseDrivers/internalElementBaseDriver';
