5374c320ac1a5b55040cc4a8d66f080f7acd0ff4
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-02-16 15:05:34 +0200

    change output type (#51)
    * change output type #pr
    
    * fix desc

--- src/element/getProperty.ts
+++ src/element/getProperty.ts
@@ -8,7 +8,6 @@ import {
 
 /**
  * Gets the element property value.
- * If the property value is absent returns `null`.
  * If a selector was passed then the method tries to find the element and only then returns the property value.
  */
 export async function getProperty(
@@ -16,17 +15,15 @@ export async function getProperty(
   context: DocumentContext,
   selectorOrElement: SelectorOrElement,
   searchElementOptions?: SearchElementOptions,
-): Promise<string | null> {
+) {
   const element = await getElement(
     context,
     selectorOrElement,
     searchElementOptions,
   );
-
-  const result = await context.evaluate(
-    (e, elementProperty) => (e[elementProperty] ? e[elementProperty] : null),
+  return context.evaluate(
+    (e, elementProperty) => e[elementProperty],
     element,
     property,
   );
-  return result;
 }
