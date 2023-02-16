8a692be756ec3bdc7270999bbcba66499f3015fc
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-02-16 15:29:07 +0200

    rm boolean from type (#52)
    * rm boolean from type #pr
    
    * rm boolean from type in correct place

--- src/waits/waitForCondition.ts
+++ src/waits/waitForCondition.ts
@@ -2,7 +2,7 @@ import { WaitOptions } from '../types';
 import { waitFor } from '../waitFor';
 
 export async function waitForCondition(
-  action: () => Promise<boolean> | boolean,
+  action: () => Promise<boolean>,
   waitOptions?: WaitOptions,
   exceptionMessage?: string,
   withCallee?: () => any,
