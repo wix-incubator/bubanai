55854ee95ec1570f654cea8f761e9034f6e6dcfd
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-02-28 16:24:27 +0200

    add descriptions (#59)
    * add descriptions #pr
    
    * change description

--- src/console/getCopiedText.ts
+++ src/console/getCopiedText.ts
@@ -1,5 +1,11 @@
 import { DocumentContext } from '../page';
 
+/**
+ * Returns copied text from clipboard.
+ * Needs clipboard permissions to the page.
+ * (for more details https://github.com/chromium/chromium/commit/f66617497ff714042afe98ce19a7cb611e08c07d)
+ * @param context Page or Frame
+ */
 export async function getCopiedText(context: DocumentContext): Promise<string> {
   return context.evaluate(() => navigator.clipboard.readText());
 }
--- src/drag/dragBy.ts
+++ src/drag/dragBy.ts
@@ -2,6 +2,14 @@ import { Page, Point } from 'puppeteer-core';
 import { dragTo } from './dragTo';
 import { DragOptions } from './types';
 
+/**
+ * Drags element from point to point with offset by defined coordinates.
+ * @param page Page or Frame
+ * @param from Coordinates of the drag object
+ * @param byX Left offset
+ * @param byY Top offset
+ * @param options DragOptions
+ */
 export async function dragBy(
   page: Page,
   from: Point,
