09d41b396e87c26a8cb8361bd8ea22559126725f
yaroslavm <yaroslavm@wix.com>
2023-03-29 19:29:21 +0300

    add 2 methods #pr

---
+++ src/element/waits/waitForElementToHavePropertyPart.ts
@@ -0,0 +1,38 @@
+import { DocumentContext } from '../../page';
+import { ElementPropertyType, SelectorOrElement } from '../types';
+import { DefaultWaitOptions, WaitOptions } from '../../types';
+import { getProperty } from '../getProperty';
+import { waitFor } from '../../waitFor';
+
+/**
+ * Waits for element to have property value part.
+ * @param context Page or Frame
+ * @param element Element or selector
+ * @param property Property name
+ * @param value Property string value part
+ * @param waitOptions WaitOptions
+ *
+ * @example `await element.type('1000 dsdsd');` <br>
+ * `await waitForElementToHavePropertyPart(page, element, 'innerHtml', 'dsdsd');`
+ *
+ * @category Element Waits
+ */
+export function waitForElementToHavePropertyPart(
+  context: DocumentContext,
+  element: SelectorOrElement,
+  property: ElementPropertyType,
+  value: string,
+  waitOptions?: WaitOptions,
+) {
+  const propertyFunc = async () =>
+    getProperty(property, context, element).then((p) =>
+      p.toString().includes(value),
+    );
+  return waitFor(
+    propertyFunc,
+    waitOptions,
+    `Element doesn't have property ${property} with part ${value} after timeout ${
+      waitOptions?.timeoutMs || DefaultWaitOptions.timeoutMs
+    } s`,
+  );
+}
