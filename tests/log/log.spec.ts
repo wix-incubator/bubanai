ecca37c3845fef2ecfc09946d8237d2dac4bdea0
YaroslavMindolin <y.m.mindolin@gmail.com>
2023-04-28 17:29:04 +0300

    add log util units

---
+++ tests/log/log.spec.ts
@@ -0,0 +1,141 @@
+import { log, makeLoggable } from '../../src';
+import { ConsoleLogger } from '../../src/log/loggerFactory';
+import { Page } from 'puppeteer';
+
+type ComplicatedObject = {
+  property1: {
+    property2: {
+      _page: Record<string, unknown>;
+      property3: {
+        class: string;
+        spec: LogSpec;
+      };
+    };
+    array: string[];
+  };
+};
+
+class LogSpec {
+  @log
+  async method1() {
+    return null;
+  }
+
+  @log
+  async withArguments(
+    str: string,
+    array: (string | number)[],
+    obj: { a: boolean },
+  ) {
+    return obj;
+  }
+
+  @log
+  async withComplicatedObject(obj: ComplicatedObject) {
+    return obj;
+  }
+
+  @log
+  async circularStructure(obj: { _page: Page }) {
+    return obj._page;
+  }
+
+  @log
+  async pageObject(_page: Page) {
+    return _page;
+  }
+}
+describe('Log: @log', () => {
+  let loggerSpy: jest.SpyInstance<void>;
+  const classInstance = LogSpec;
+
+  beforeEach(() => {
+    // Replace the logger with a spy before each test
+    loggerSpy = jest.spyOn(ConsoleLogger.prototype, 'log').mockImplementation();
+  });
+
+  afterEach(() => {
+    // Restore the original logger after each test
+    loggerSpy.mockRestore();
+  });
+
+  it('logs method name without arguments on function call', async () => {
+    const result = await new classInstance().method1();
+
+    expect(loggerSpy).toHaveBeenCalledWith(
+      expect.stringContaining(`Calling ${classInstance.name}.method1();`),
+    );
+    expect(result).toBe(null);
+  });
+
+  it('logs method name with arguments on function call', async () => {
+    const firstArg = '1';
+    const secondArg = [1, '2'];
+    const thirdArg = { a: true };
+    const result = await new classInstance().withArguments(
+      firstArg,
+      secondArg,
+      thirdArg,
+    );
+
+    expect(loggerSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        `Calling ${classInstance.name}.withArguments(${JSON.stringify(
+          firstArg,
+        )}, ${JSON.stringify(secondArg)}, ${JSON.stringify(thirdArg)});`,
+      ),
+    );
+    expect(result).toBe(thirdArg);
+  });
+
+  it('logs method name with complicated object', async () => {
+    const className = 'myClass 1';
+    const array = ['1', '2', '3'];
+    const complicatedObject = makeLoggable({
+      OBJECT_CONTAINER: {
+        property1: {
+          property2: {
+            _page: { fn: jest.fn() },
+            property3: {
+              class: className,
+              spec: new LogSpec(),
+            },
+          },
+          array,
+        },
+      },
+    });
+    const result = await new classInstance().withComplicatedObject(
+      complicatedObject.OBJECT_CONTAINER,
+    );
+
+    expect(loggerSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        `Calling ${classInstance.name}.withComplicatedObject(OBJECT_CONTAINER);`,
+      ),
+    );
+    expect(result).toEqual(complicatedObject.OBJECT_CONTAINER);
+  });
+
+  it('should not explode on objects with circular structure', async () => {
+    const result = await new classInstance().circularStructure({ _page: page });
+
+    expect(loggerSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        `Calling ${classInstance.name}.circularStructure(Object);`,
+      ),
+    );
+    expect(result).toBe(page);
+  });
+
+  it('should return className for complicated object with defined constructor name', async () => {
+    const result = await new classInstance().pageObject(page);
+
+    expect(loggerSpy).toHaveBeenCalledWith(
+      expect.stringContaining(
+        `Calling ${classInstance.name}.pageObject(Page);`,
+      ),
+    );
+    expect(result).toBe(page);
+  });
+});
