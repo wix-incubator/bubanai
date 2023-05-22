8648dbea076ba3ec60ec7024e24bd674f12892ba
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-05-22 18:57:07 +0300

    add units for waits (#79)

---
+++ tests/waits/waitForValueToBeEqualOrLessThan.spec.ts
@@ -0,0 +1,76 @@
+import { TestError, wait, waitForValueToBeEqualOrLessThan } from '../../src';
+import { wrapError } from './waitUtils.testKit';
+
+describe('Waits: waitForValueToBeEqualOrLessThan()', () => {
+  it('resolves if function value equals expected after wait', async () => {
+    let result = 0.9;
+    const decrement = 0.1;
+    const expectedValue = result - decrement;
+    const actionTimeout = 1000;
+    const pollIntervalMs = 500;
+    const changeVarFunc = async () => {
+      await wait(actionTimeout);
+      result -= decrement;
+      return result;
+    };
+    const numberReturnFunc = jest.fn(() => Promise.resolve(result));
+    changeVarFunc();
+    await expect(
+      waitForValueToBeEqualOrLessThan(numberReturnFunc, expectedValue, {
+        pollIntervalMs,
+      }),
+    ).resolves.toBeUndefined();
+    expect(numberReturnFunc).toHaveBeenCalledTimes(
+      actionTimeout / pollIntervalMs + 1,
+    );
+  });
+
+  it('resolves if function value is less than expected after wait', async () => {
+    let result = 0.9;
+    const decrement = 0.1;
+    const expectedValue = 0.89;
+    const actionTimeout = 1000;
+    const pollIntervalMs = 500;
+    const changeVarFunc = async () => {
+      await wait(actionTimeout);
+      result -= decrement;
+      return result;
+    };
+    const numberReturnFunc = jest.fn(() => Promise.resolve(result));
+    changeVarFunc();
+    await expect(
+      waitForValueToBeEqualOrLessThan(numberReturnFunc, expectedValue, {
+        pollIntervalMs,
+      }),
+    ).resolves.toBeUndefined();
+    expect(numberReturnFunc).toHaveBeenCalledTimes(
+      actionTimeout / pollIntervalMs + 1,
+    );
+  });
+
+  it('rejects if function value is more than expected after wait', async () => {
+    const result = 0.9;
+    const expectedValue = result - 0.00001;
+    const numberReturnFunc = jest.fn(async () => result);
+    const timeoutMs = 1000;
+    const pollIntervalMs = 500;
+    await expect(
+      waitForValueToBeEqualOrLessThan(numberReturnFunc, expectedValue, {
+        timeoutMs,
+        pollIntervalMs,
+      }),
+    ).rejects.toThrowError(
+      wrapError(
+        await TestError.ValueIsEqualOrLessThan(
+          numberReturnFunc,
+          expectedValue,
+          timeoutMs,
+        ),
+        numberReturnFunc,
+      ),
+    );
+    expect(numberReturnFunc).toHaveBeenCalledTimes(
+      timeoutMs / pollIntervalMs + 2,
+    );
+  });
+});
---
+++ tests/waits/waitForValueToBeEqualOrMoreThan.spec.ts
@@ -0,0 +1,76 @@
+import { TestError, wait, waitForValueToBeEqualOrMoreThan } from '../../src';
+import { wrapError } from './waitUtils.testKit';
+
+describe('Waits: waitForValueToBeEqualOrMoreThan()', () => {
+  it('resolves if function value equals expected after wait', async () => {
+    let result = 0.89;
+    const decrement = 0.01;
+    const expectedValue = result + decrement;
+    const actionTimeout = 1000;
+    const pollIntervalMs = 500;
+    const changeVarFunc = async () => {
+      await wait(actionTimeout);
+      result += decrement;
+      return result;
+    };
+    const numberReturnFunc = jest.fn(() => Promise.resolve(result));
+    changeVarFunc();
+    await expect(
+      waitForValueToBeEqualOrMoreThan(numberReturnFunc, expectedValue, {
+        pollIntervalMs,
+      }),
+    ).resolves.toBeUndefined();
+    expect(numberReturnFunc).toHaveBeenCalledTimes(
+      actionTimeout / pollIntervalMs + 1,
+    );
+  });
+
+  it('resolves if function value is more than than expected after wait', async () => {
+    let result = 0.89;
+    const decrement = 0.1;
+    const expectedValue = 0.9;
+    const actionTimeout = 1000;
+    const pollIntervalMs = 500;
+    const changeVarFunc = async () => {
+      await wait(actionTimeout);
+      result += decrement;
+      return result;
+    };
+    const numberReturnFunc = jest.fn(() => Promise.resolve(result));
+    changeVarFunc();
+    await expect(
+      waitForValueToBeEqualOrMoreThan(numberReturnFunc, expectedValue, {
+        pollIntervalMs,
+      }),
+    ).resolves.toBeUndefined();
+    expect(numberReturnFunc).toHaveBeenCalledTimes(
+      actionTimeout / pollIntervalMs + 1,
+    );
+  });
+
+  it('rejects if function value is more than expected after wait', async () => {
+    const result = 0.9;
+    const expectedValue = result + 0.00001;
+    const numberReturnFunc = jest.fn(async () => result);
+    const timeoutMs = 1000;
+    const pollIntervalMs = 500;
+    await expect(
+      waitForValueToBeEqualOrMoreThan(numberReturnFunc, expectedValue, {
+        timeoutMs,
+        pollIntervalMs,
+      }),
+    ).rejects.toThrowError(
+      wrapError(
+        await TestError.ValueIsEqualOrMoreThan(
+          numberReturnFunc,
+          expectedValue,
+          timeoutMs,
+        ),
+        numberReturnFunc,
+      ),
+    );
+    expect(numberReturnFunc).toHaveBeenCalledTimes(
+      timeoutMs / pollIntervalMs + 2,
+    );
+  });
+});
