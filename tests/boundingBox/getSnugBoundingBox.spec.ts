39095f69094f4d75f528c1b1e93356bbeec8259d
YaroslavMindolin <y.m.mindolin@gmail.com>
2023-06-07 12:10:42 +0300

    add units for bounding

---
+++ tests/boundingBox/getSnugBoundingBox.spec.ts
@@ -0,0 +1,30 @@
+import { AtLeastTwoValuesArray, getSnugBoundingBox } from '../../src';
+import { BoundingBox } from 'puppeteer-core';
+
+describe('Bounding Box: getSnugBoundingBox()', () => {
+  it('should calculate the snug bounding box of multiple bounding boxes', () => {
+    const boundingBoxes: AtLeastTwoValuesArray<BoundingBox> = [
+      { x: 10, y: 20, width: 100, height: 200 },
+      { x: 50, y: 30, width: 150, height: 100 },
+      { x: 20, y: 40, width: 120, height: 180 },
+    ];
+
+    const leftMost = Math.min(...boundingBoxes.map((box) => box.x));
+    const topMost = Math.min(...boundingBoxes.map((box) => box.y));
+    const rightMost = Math.max(
+      ...boundingBoxes.map((box) => box.x + box.width),
+    );
+    const bottomMost = Math.max(
+      ...boundingBoxes.map((box) => box.y + box.height),
+    );
+
+    const result = getSnugBoundingBox(boundingBoxes);
+
+    expect(result).toEqual({
+      x: leftMost,
+      y: topMost,
+      width: rightMost - leftMost,
+      height: bottomMost - topMost,
+    });
+  });
+});
