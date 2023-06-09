043bf4bc4a6c90f625227895ac1720ffda31f533
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-06-09 14:00:10 +0300

    element states units part 1 (#89)

---
+++ tests/element/states/isBold.spec.ts
@@ -0,0 +1,36 @@
+import { isBold } from '../../../src';
+
+describe('Element States: isBold()', () => {
+  beforeAll(async () => {
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            #boldElement {
+              font-weight: bold;
+            }
+            #normalElement {
+              font-weight: normal;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="boldElement">This is bold text.</div>
+          <div id="normalElement">This is normal text.</div>
+        </body>
+      </html>
+    `;
+
+    await page.setContent(htmlContent);
+  });
+
+  it('should return true if element computed font weight is 700', async () => {
+    const result = await isBold(page as never, '#boldElement');
+    expect(result).toBe(true);
+  });
+
+  it('should return false if element computed font weight is not 700', async () => {
+    const result = await isBold(page as never, '#normalElement');
+    expect(result).toBe(false);
+  });
+});
---
+++ tests/element/states/isItalic.spec.ts
@@ -0,0 +1,36 @@
+import { isItalic } from '../../../src';
+
+describe('Element States: isItalic()', () => {
+  beforeAll(async () => {
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            #italicElement {
+              font-style: italic;
+            }
+            #normalElement {
+              font-style: normal;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="italicElement">This is italic text.</div>
+          <div id="normalElement">This is normal text.</div>
+        </body>
+      </html>
+    `;
+
+    await page.setContent(htmlContent);
+  });
+
+  it('should return true if element computed font style is italic', async () => {
+    const result = await isItalic(page as never, '#italicElement');
+    expect(result).toBe(true);
+  });
+
+  it('should return false if element computed font style is not italic', async () => {
+    const result = await isItalic(page as never, '#normalElement');
+    expect(result).toBe(false);
+  });
+});
