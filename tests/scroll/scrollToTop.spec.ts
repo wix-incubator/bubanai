b98488676426364c8a419b1d7d3afd7873f3d39d
YaroslavMindolin <y.m.mindolin@gmail.com>
2023-06-14 18:59:50 +0300

    add 2 units

---
+++ tests/scroll/scrollToTop.spec.ts
@@ -0,0 +1,32 @@
+import { scrollTo, scrollToTop } from '../../src';
+
+jest.mock('../../src/scroll/scrollTo', () => ({
+  ...jest.requireActual('../../src/scroll/scrollTo'),
+  scrollTo: jest.fn(),
+}));
+describe('Scroll: scrollToTop()', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call scroll to 0 by height', async () => {
+    await page.setContent(`<html>
+            <head>
+              <style>
+                .bigElement {
+                  height: 200vh;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="bigElement">
+              </div>
+            </body>
+            </html>`);
+
+    await scrollToTop(page as never);
+    const scrollY = await page.evaluate(() => window.scrollY);
+    expect(scrollTo).toHaveBeenCalledWith(page as never, 0);
+    expect(scrollY).toBe(0);
+  });
+});
