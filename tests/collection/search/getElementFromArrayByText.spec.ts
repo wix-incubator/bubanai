8c18f5cb9626f172b519ffb34c41ea3165bd9290
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-06-19 13:29:06 +0300

    add collections units (#104)

---
+++ tests/collection/search/getElementFromArrayByText.spec.ts
@@ -0,0 +1,54 @@
+import { getElementFromArrayByText, getText, TestError } from '../../../src';
+
+describe('Collections Search: getElementFromArrayByText()', () => {
+  beforeAll(async () => {
+    await page.setContent(
+      `<html>
+        <head>
+        </head>
+        <body>
+          <div>Text1</div>
+          <div>Text21</div>
+          <div>Text3</div>
+        </body>
+      </html>`,
+    );
+  });
+
+  it('should return the element with the exact matching text', async () => {
+    const text = 'Text21';
+    const ignoreCase = false;
+
+    const result = await getElementFromArrayByText(
+      page as never,
+      'div',
+      text,
+      ignoreCase,
+    );
+
+    expect(await getText(page as never, result)).toBe(text);
+  });
+
+  it('should return the element with the exact matching text (ignore case)', async () => {
+    const text = 'text1';
+    const ignoreCase = true;
+
+    const result = await getElementFromArrayByText(
+      page as never,
+      'div',
+      text,
+      ignoreCase,
+    );
+
+    expect(await getText(page as never, result)).toBe('Text1');
+  });
+
+  it('should throw an error if the element with the exact matching text is not found', async () => {
+    const text = 'Text2';
+    const ignoreCase = false;
+
+    await expect(
+      getElementFromArrayByText(page as never, 'div', text, ignoreCase),
+    ).rejects.toThrowError(TestError.ExactTextIsNotFoundInArray(text));
+  });
+});
