7fddc142c67e8ad5bc52d8f0a18a3c3f373756b2
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-06-29 13:13:45 +0300

    add units for element props (#115)

---
+++ tests/element/props/getClasses.spec.ts
@@ -0,0 +1,27 @@
+import { getClasses } from '../../../src';
+
+describe('Element Properties: getClasses()', () => {
+  beforeAll(async () => {
+    const htmlContent = `
+      <html>
+        <head></head>
+        <body>
+          <div id="element1" class="class1  class2 class3"></div>
+          <div id="element2"></div>
+        </body>
+      </html>
+    `;
+
+    await page.setContent(htmlContent);
+  });
+
+  it('should return an array of classes for the specified element', async () => {
+    const result = await getClasses(page as never, '#element1');
+    expect(result).toEqual(['class1', 'class2', 'class3']);
+  });
+
+  it('should return an empty array if the element has no classes', async () => {
+    const result = await getClasses(page as never, '#element2');
+    expect(result).toEqual([]);
+  });
+});
