b3627da09f9e9190d900e6fb3bab55a434bfe9d7
YaroslavMindolin <y.m.mindolin@gmail.com>
2023-06-30 11:18:16 +0300

    add units for element props 4

---
+++ tests/element/props/getElementBackgroundColor.spec.ts
@@ -0,0 +1,40 @@
+import { getElementBackgroundColor } from '../../../src';
+
+describe('Element Properties: getElementBackgroundColor()', () => {
+  beforeAll(async () => {
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            .element {
+              font-size: 16px;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="element1" class="element">Hello World</div>
+          <div id="element2" class="element">Hello World</div>
+              <script>
+                const el = document.querySelector('.element');
+                el.style.backgroundColor = 'rgb(255, 1, 1)';
+              </script>
+        </body>
+      </html>
+    `;
+
+    await page.setContent(htmlContent);
+  });
+
+  it('should return the background color style property in RGB format value for the specified element', async () => {
+    const result = await getElementBackgroundColor(
+      page as never,
+      `//*[@id='element1']`,
+    );
+    expect(result).toEqual({ r: 255, g: 1, b: 1 });
+  });
+
+  it('should return undefined string if the background color property is not set', async () => {
+    const result = await getElementBackgroundColor(page as never, '#element2');
+    expect(result).toBeUndefined();
+  });
+});
---
+++ tests/element/props/getElementStyleAsRGB.spec.ts
@@ -0,0 +1,53 @@
+import { getElementStyleAsRGB } from '../../../src';
+
+describe('Element Properties: getElementStyleAsRGB()', () => {
+  beforeAll(async () => {
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            .element {
+              font-size: 16px;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="element1" class="element">Hello World</div>
+              <script>
+                const el = document.querySelector('.element');
+                el.style.color = 'rgb(255, 1, 1)';
+              </script>
+        </body>
+      </html>
+    `;
+
+    await page.setContent(htmlContent);
+  });
+
+  it('should return the style property in RGB format value for the specified element', async () => {
+    const result = await getElementStyleAsRGB(
+      page as never,
+      '#element1',
+      'color',
+    );
+    expect(result).toEqual({ r: 255, g: 1, b: 1 });
+  });
+
+  it('should return undefined string if the style property is not set', async () => {
+    const result = await getElementStyleAsRGB(
+      page as never,
+      '#element1',
+      'background-color',
+    );
+    expect(result).toBeUndefined();
+  });
+
+  it('should undefined for non-existing style', async () => {
+    const result = await getElementStyleAsRGB(
+      page as never,
+      '#element1',
+      'any',
+    );
+    expect(result).toBeUndefined();
+  });
+});
