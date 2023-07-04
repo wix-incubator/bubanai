bbca7c688770e494a31cdd46886fed30cb036e9c
Yaroslavm <13870487+YaroslavMindolin@users.noreply.github.com>
2023-07-04 10:57:23 +0300

    add units element actions 2 (#119)

--- src/element/actions/clickOnParent.ts
+++ src/element/actions/clickOnParent.ts
@@ -1,17 +1,20 @@
 import type { DocumentContext } from '../../page';
 import { getParent } from './getParent';
 import type { SelectorOrElement } from '../types';
+import type { ClickOptions } from 'puppeteer-core';
 
 /**
  * Clicks on parent element of current element
  * @param context Page or Frame
  * @param elementOrSelector Element or selector
+ * @param options Click Options
  *
  * @category Element Actions
  */
 export async function clickOnParent(
   context: DocumentContext,
   elementOrSelector: SelectorOrElement,
+  options?: ClickOptions,
 ): Promise<void> {
-  return getParent(context, elementOrSelector).then((p) => p.click());
+  return getParent(context, elementOrSelector).then((p) => p.click(options));
 }
---
+++ tests/element/actions/clickAtOffset.spec.ts
@@ -0,0 +1,40 @@
+import { clickAtOffset, getElement } from '../../../src';
+
+describe('Element Actions: clickAtOffset()', () => {
+  let container;
+  let element;
+  beforeAll(async () => {
+    await page.setContent(`
+      <html>
+        <head>
+          <title>Test Page</title>
+        </head>
+        <body>
+          <div id="container" style="position: relative; width: 200px; height: 200px;">
+            <button id="element" style="position: absolute; top: 50px; left: 50px;" onclick="changeText()">Click Me</button>
+          </div>
+
+          <script>
+            function changeText() {
+              var button = document.getElementById('element');
+              button.innerText = 'Clicked';
+            }
+          </script>
+        </body>
+      </html>
+    `);
+    container = await getElement(page as never, '#container');
+    element = await getElement(page as never, '//button');
+  });
+
+  it('should click at the specified offset from the element', async () => {
+    const offsetX = 51;
+    const offsetY = 51;
+
+    await clickAtOffset(page as never, container, { offsetX, offsetY });
+
+    // Verify the click action by checking the button's text
+    const buttonText = await page.evaluate((el) => el.innerText, element);
+    expect(buttonText).toBe('Clicked');
+  });
+});
