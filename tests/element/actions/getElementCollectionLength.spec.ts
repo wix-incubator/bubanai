d04dd3ddd5f0fe136af666078d4ff0f2040b0935
YaroslavMindolin <y.m.mindolin@gmail.com>
2023-07-06 12:26:54 +0300

    add units element actions 5 + fix interface

---
+++ tests/element/actions/getElementCollectionLength.spec.ts
@@ -0,0 +1,49 @@
+import { getElementsCollectionLength, getElements } from '../../../src';
+
+describe('Element Actions: getElementsCollectionLength()', () => {
+  beforeAll(async () => {
+    await page.reload();
+    await page.setContent(`
+      <html>
+        <head>
+          <title>Test Page</title>
+        </head>
+        <body>
+          <div class="element">Element 1</div>
+          <div class="element" style="visibility: hidden;">Element 2</div>
+          <div class="element">Element 3</div>
+        </body>
+      </html>'
+    `);
+  });
+
+  it('should return the correct number of elements', async () => {
+    const elements = await getElements(page as never, '.element');
+    const length = await getElementsCollectionLength(page as never, elements);
+    expect(length).toBe(3);
+  });
+
+  it('should return 0 when no elements are found', async () => {
+    const length = await getElementsCollectionLength(
+      page as never,
+      '.non-existent-element',
+    );
+    expect(length).toBe(0);
+  });
+
+  it('should return 0 when an empty array of selectors is provided', async () => {
+    const length = await getElementsCollectionLength(page as never, []);
+    expect(length).toBe(0);
+  });
+
+  it('should return the correct number of elements using search options', async () => {
+    const searchOptions = { shouldBeNotEmpty: true };
+    setTimeout(() => page.setContent('<div class="element1"></div>'), 2000);
+    const length = await getElementsCollectionLength(
+      page as never,
+      '.element1',
+      searchOptions,
+    );
+    expect(length).toBe(1);
+  });
+});
